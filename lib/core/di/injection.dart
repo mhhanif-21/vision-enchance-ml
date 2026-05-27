2350d7dd70961dd064f22575b6d4757723dffa8c
muhammad hanif <muhammad.hanif.tif24@polban.ac.id>
2026-05-27 21:23:17 +0700

    refactor(di): setup getit dependency injection untuk clean architecture

--- lib/core/di/injection.dart
+++ lib/core/di/injection.dart
@@ -6,6 +6,19 @@ import '../../services/ml/onnx_inference_service.dart';
 import '../../services/ml/tile_processor.dart';
 import '../../services/storage/file_storage_service.dart';
 
+// Repositories
+import '../../features/history/domain/repositories/i_history_repository.dart';
+import '../../features/history/data/repositories/history_repository_impl.dart';
+import '../../features/restore/domain/repositories/i_restore_repository.dart';
+
+// UseCases
+import '../../features/history/domain/usecases/manage_history_usecase.dart';
+import '../../features/restore/domain/usecases/restore_image_usecase.dart';
+
+// Blocs
+import '../../features/history/presentation/bloc/history_bloc.dart';
+import '../../features/restore/presentation/bloc/restore_bloc.dart';
+
 final sl = GetIt.instance;
 
 Future<void> initDependencies() async {
@@ -14,7 +27,7 @@ Future<void> initDependencies() async {
   sl.registerLazySingleton<ImagePreprocessor>(() => ImagePreprocessor());
   sl.registerLazySingleton<ImagePostprocessor>(() => ImagePostprocessor());
   sl.registerLazySingleton<TileProcessor>(() => TileProcessor());
-  sl.registerLazySingleton<OnnxInferenceService>(
+  sl.registerLazySingleton<IRestoreRepository>(
     () => OnnxInferenceService(
       modelManager: sl<ModelManager>(),
       preprocessor: sl<ImagePreprocessor>(),
@@ -23,6 +36,17 @@ Future<void> initDependencies() async {
     ),
   );
 
-  // Storage
+  // Storage Services
   sl.registerLazySingleton<FileStorageService>(() => FileStorageService());
+
+  // Repositories
+  sl.registerLazySingleton<IHistoryRepository>(() => HistoryRepositoryImpl());
+
+  // UseCases
+  sl.registerLazySingleton<RestoreImageUseCase>(() => RestoreImageUseCase(sl<IRestoreRepository>()));
+  sl.registerLazySingleton<ManageHistoryUseCase>(() => ManageHistoryUseCase(sl<IHistoryRepository>()));
+
+  // BLoCs
+  sl.registerFactory<RestoreBloc>(() => RestoreBloc(useCase: sl<RestoreImageUseCase>()));
+  sl.registerFactory<HistoryBloc>(() => HistoryBloc(useCase: sl<ManageHistoryUseCase>()));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'core/storage/hive_setup.dart';
-import 'features/history/data/repositories/history_repository_impl.dart';
-import 'services/ml/onnx_inference_service.dart';
-import 'services/ml/model_manager.dart';
-import 'services/ml/image_preprocessor.dart';
-import 'services/ml/image_postprocessor.dart';
-import 'services/ml/tile_processor.dart';
+import 'core/di/injection.dart';
 import 'features/history/presentation/bloc/history_bloc.dart';
 import 'features/restore/presentation/bloc/restore_bloc.dart';
+import 'app/app.dart';
 import 'app/app.dart';
 
 void main() async {
@@ -19,30 +15,18 @@ void main() async {
   // Menginisialisasi penyimpanan lokal (Hive)
   await HiveSetup.init();
   
-  // Menginisialisasi service ML
-  final modelManager = ModelManager();
-  final preprocessor = ImagePreprocessor();
-  final postprocessor = ImagePostprocessor();
-  final tileProcessor = TileProcessor();
-  
-  final onnxService = OnnxInferenceService(
-    modelManager: modelManager,
-    preprocessor: preprocessor,
-    postprocessor: postprocessor,
-    tileProcessor: tileProcessor,
-  );
-
-  final historyRepo = HistoryRepositoryImpl();
+  // Menginisialisasi dependencies via GetIt
+  await initDependencies();
 
   // Menjalankan aplikasi dengan MultiBlocProvider agar state dapat diakses secara global
   runApp(
     MultiBlocProvider(
       providers: [
         BlocProvider(
-          create: (_) => HistoryBloc(repository: historyRepo)..add(LoadHistory()),
+          create: (_) => sl<HistoryBloc>()..add(LoadHistory()),
         ),
         BlocProvider(
-          create: (_) => RestoreBloc(inferenceService: onnxService),
+          create: (_) => sl<RestoreBloc>(),
         ),
       ],
       child: const LuminaRestoreApp(),
